32e788432bc60cc4513ce46780eb0af07f16ac7a
DELL <dev.mahmoudtarek@gmail.com>
2026-07-07 18:07:44 +0300

    Implement `get_time_ago` dependency and refactor Home screen structure
    - Add `get_time_ago` dependency to `pubspec.yaml`
    - Refactor `Home` screen to improve layout using `Expanded` for `CategoryDetails`
    - Update `AppBar` styling in `Home` screen and set `surfaceTintColor` to transparent
    - Move `CategoryDetails` widget to a new directory structure under `ui/screens/home/sources/`
    - Remove unused `getSources` helper function and redundant imports from `home.dart`
    - Add a newline for code consistency in `MainLoading` widget

--- lib/ui/screens/home/home.dart
+++ lib/ui/screens/home/home.dart
@@ -1,6 +1,4 @@
-import 'package:a5bark/data/api/api_manager.dart';
-import 'package:a5bark/model/sources_response.dart';
-import 'package:a5bark/ui/screens/home/widgets/source/category_details.dart';
+import 'package:a5bark/ui/screens/home/sources/category_details.dart';
 import 'package:a5bark/utils/resources/app_text_style.dart';
 import 'package:flutter/material.dart';
 
@@ -19,23 +17,16 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('A5bark'),
+        surfaceTintColor: Colors.transparent,
+        title: Text(
+          'A5bark',
+          style: AppTextStyle.bodyLarge.copyWith(fontSize: 20),
+        ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 10),
-          CategoryDetails()
-]
-      ),
+      body: Column(children: [Expanded(child: CategoryDetails())]),
     );
   }
-}
-
-Future<List<Source>> getSources() async {
-  var response = await ApiManager().getSources();
-  return response.sources!;
 }
