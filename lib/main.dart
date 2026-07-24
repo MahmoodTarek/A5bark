be4b18127a07cfbd8d954791ca7dafc210f0ba46
DELL <dev.mahmoudtarek@gmail.com>
2026-07-24 18:32:20 +0300

    Implement offline caching for articles using Hive and connectivity_plus
    - Implement `ArticlesLocalDataSource` and its Hive-based implementation `ArticlesLocalDataSourceImpl`
    - Update `ArticlesRepositoryImpl` to handle data retrieval based on network connectivity
    - Cache remote articles locally upon successful fetch and retrieve from cache when offline
    - Initialize Hive in `main.dart` using `path_provider` for storage directory
    - Update `ArticlesCubit` to inject the new `ArticlesLocalDataSource` into the repository
    - Add `connectivity_plus`, `hive_ce`, and `path_provider` dependencies to `pubspec.yaml`

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:a5bark/a5bark_app.dart';
 import 'package:a5bark/data/shared_pref/shared_pref.dart';
 import 'package:a5bark/theme_cubit.dart';
@@ -5,10 +7,15 @@ import 'package:a5bark/utils/bloc_observer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_ce/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentsDir.path);
 
   final isDark = await AppLaunchService.isDarkTheme();
   Bloc.observer = MyBlocObserver();
--- lib/ui/screens/home/sources/articles/cubit/articles_cubit.dart
+++ lib/ui/screens/home/sources/articles/cubit/articles_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:a5bark/data/api/api_manager.dart';
 import 'package:a5bark/data/repository/articles/data_sources/impl/articles_remote_data_source_impl.dart';
+import 'package:a5bark/data/repository/articles/data_sources/local/articles_local_data_source.dart';
 import 'package:a5bark/data/repository/articles/data_sources/remote/articles_remote_data_source.dart';
 import 'package:a5bark/data/repository/articles/repository/articles_repository.dart';
 import 'package:a5bark/data/repository/articles/repository/impl/articles_repository_impl.dart';
@@ -12,11 +13,15 @@ class ArticlesCubit extends Cubit<ArticlesState> {
   late final ApiManager apiManager;
   late final ArticlesRepository articlesRepository;
   late final ArticlesRemoteDataSource articlesRemoteDataSource;
+  late final ArticlesLocalDataSource articlesLocalDataSource;
 
   ArticlesCubit() : super(ArticlesLoadingState()) {
     apiManager = ApiManager(Dio());
     articlesRemoteDataSource = ArticlesRemoteDataSourceImpl(apiManager);
-    articlesRepository = ArticlesRepositoryImpl(articlesRemoteDataSource);
+    articlesRepository = ArticlesRepositoryImpl(
+      articlesRemoteDataSource: articlesRemoteDataSource,
+      articlesLocalDataSource: articlesLocalDataSource,
+    );
   }
 
   Future<void> getArticles({required String sourceId}) async {
